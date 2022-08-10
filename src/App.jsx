8351c2e176b3bdf50fe30a43b7cf0c288717c0ac
felpfsf <feferreira.sf@gmail.com>
2022-08-10 17:42:10 -0300

    refactoring routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
-import { Outlet } from 'react-router-dom'
+// React
+import { Routes, Route } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+// Components
 import Navbar from './components/navbar/Navbar'
+// Pages
+import Account from './pages/Account'
+import Home from './pages/Home'
 
 function App() {
+  const API_URL = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=brl&order=market_cap_desc&per_page=10&page=1&sparkline=true'
+  const [crypto, setCrypto] = useState([])
+
+  useEffect(() => {
+    axios.get(API_URL).then(res => {
+      setCrypto(res.data)
+      // console.log(res.data)
+    }).catch(err => console.log(err))
+  }, [API_URL])
+
   return (
-    <div>
+    <>
       <Navbar />
-      <Outlet />
-    </div>
+      <Routes>
+        <Route path={'/'} element={<Home crypto={crypto} />} />
+        <Route path={'/account'} element={<Account />} />
+      </Routes>
+    </>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,22 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-// Routes
-import Home from './pages/Home'
-import Account from './pages/Account'
-
 // CSS
 import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route element={<App />}>
-          <Route path={'/'} element={<Home />} />
-          <Route path={'/account'} element={<Account />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    {/* <React.StrictMode> */}
+      <App />
+    {/* </React.StrictMode> */}
+  </BrowserRouter>
 )
