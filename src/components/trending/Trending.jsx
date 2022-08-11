87bac5d518ea7fb375227660c2759a80d5297a3d
felpfsf <feferreira.sf@gmail.com>
2022-08-10 21:35:45 -0300

    added Link to details

--- src/components/trending/Trending.jsx
+++ src/components/trending/Trending.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const Trending = () => {
   const URL_API_TRENDING = 'https://api.coingecko.com/api/v3/search/trending'
@@ -18,21 +19,23 @@ const Trending = () => {
       <h2 className='text-2xl tracking-widest font-black py-4'>Trending</h2>
       <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-3'>
         {trending.map((tcoin, idx) => (
-          <div key={idx} className='max-w-[1140px] w-full mx-auto p-4 px-2 border rounded-lg flex justify-between'>
-            <div className='w-full flex items-center justify-between'>
-              <div className='flex'>
-                <img className='mr-4 rounded-full' src={tcoin.item.small} alt={tcoin.item.name} />
-                <div>
-                  <p className='font-semibold'>{tcoin.item.name}</p>
-                  <p>{tcoin.item.symbol}</p>
+          <Link key={idx} to={`/details/${tcoin.item.id}`}>
+            <div className='max-w-[1140px] w-full mx-auto p-4 px-2 border rounded-lg flex justify-between'>
+              <div className='w-full flex items-center justify-between'>
+                <div className='flex'>
+                  <img className='mr-4 rounded-full' src={tcoin.item.small} alt={tcoin.item.name} />
+                  <div>
+                    <p className='font-semibold'>{tcoin.item.name}</p>
+                    <p>{tcoin.item.symbol}</p>
+                  </div>
                 </div>
               </div>
+              <div className='mr-4 flex items-center'>
+                <img className='w-4 mr-2' src="/assets/bitcoin.webp" alt="Bitcoin symbol" />
+                <p>{tcoin.item.price_btc.toFixed(7)}</p>
+              </div>
             </div>
-            <div className='mr-4 flex items-center'>
-              <img className='w-4 mr-2' src="/assets/bitcoin.webp" alt="Bitcoin symbol" />
-              <p>{tcoin.item.price_btc.toFixed(7)}</p>
-            </div>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
