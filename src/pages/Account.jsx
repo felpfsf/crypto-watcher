641f5fc063e1ebdacabc2d5577b701663d174ecd
felpfsf <feferreira.sf@gmail.com>
2022-08-12 20:49:56 -0300

    condition to protect acc page / redirect to signin

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import { Navigate, useNavigate } from 'react-router-dom'
 import { UserAuth } from '../context/AuthContext'
 
 import FavCoins from '../components/favCoins/FavCoins'
@@ -17,27 +17,29 @@ const Account = () => {
     }
   }
 
-  return (
-    <div className='max-w-[1140px] mx-auto p-4'>
-      <div className='my-12 py-8 flex items-center justify-between'>
-        <div>
-          <h1 className='text-2xl font-bold'>Account</h1>
-          <div className='mt-4'>
-            <h2 className='text-xl font-semibold'>Welcome, {user?.email}</h2>
+  if (user) {
+    return (
+      <div className='max-w-[1140px] mx-auto p-4'>
+        <div className='my-12 py-8 flex items-center justify-between'>
+          <div>
+            <h1 className='text-2xl font-bold'>Account</h1>
+            <div className='mt-4'>
+              <h2 className='text-xl font-semibold'>Welcome, {user?.email}</h2>
+            </div>
+          </div>
+          <div>
+            <button onClick={handleLogout}>Sign Out</button>
           </div>
         </div>
-        <div>
-          <button onClick={handleLogout}>Sign Out</button>
+        <div className='max-w-[1140px] w-full mx-auto my-12 py-8 flex items-center justify-between'>
+          <div className='w-full min-h-[300px] border border-white/25 rounded-md'>
+            <h2>Crypto Watch List</h2>
+            <FavCoins />
+          </div>
         </div>
       </div>
-      <div className='max-w-[1140px] w-full mx-auto my-12 py-8 flex items-center justify-between'>
-        <div className='w-full min-h-[300px] border border-white/25 rounded-md'>
-          <h2>Crypto Watch List</h2>
-          <FavCoins />
-        </div>
-      </div>
-    </div>
-  )
+    )
+  } else { return <Navigate to={'/signin'} /> }
 }
 
 export default Account
